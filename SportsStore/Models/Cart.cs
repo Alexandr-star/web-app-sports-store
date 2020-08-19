c3623f432de947917648d08874a77d122207e73f
Alexandr-star <sanya-bass.skvortzoff@yandex.tu>
2020-08-19 22:36:39 +0300

    start create cart, create class Cart.cs

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public virtual void AddItem(Product product, int quality) {
+            CartLine cartLine = lineCollection
+                .Where(p => p.Product.ProductId == product.ProductId)
+                .FirstOrDefault();
+            if (cartLine == null)
+            {
+                lineCollection.Add(new CartLine {
+                    Product = product,
+                    Quality = quality
+                });
+            }
+            else
+            {
+                cartLine.Quality += quality;
+            }
+        }
+        public virtual void RemoveLine(Product product) =>
+            lineCollection.RemoveAll(line => line.Product.ProductId == product.ProductId);
+        public virtual decimal ComputeTotalValue() =>
+            lineCollection.Sum(element => element.Product.Price * element.Quality);
+        public virtual void Clean() => lineCollection.Clear();
+    }
+
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+        public Product Product { get; set; }
+        public int Quality { get; set; }
+
+    }
+}
